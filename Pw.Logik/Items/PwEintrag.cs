e6b2a268f359d2a6cf9d62f364fec67b4bf08442
Wolfgang Eggenhofer <wolfgang_eggenhofer@gmx.at>
2021-10-21 14:41:32 +0200

    ListCollectionView erstellt

---
+++ Pw.Logik/Items/PwEintrag.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logik.Pw.Logik.Items
+{
+    public class PwEintrag : INotifyPropertyChanged, IDataErrorInfo
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Programmname darf nicht leer sein")]
+        [MinLength(3, ErrorMessage = "Min 3 Stellen")]
+        public string Programm { get; set; }
+
+        public string Benutzer { get; set; }
+
+        public string Passwort { get; set; }
+
+        public DateTime Datum { get; set; }
+
+        public string tmprndIndex { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        #region IDataErrorInfo Management
+        public bool HasErrors => Errors.Any();
+        private Dictionary<string, string> Errors { get; } = new Dictionary<string, string>();
+        public string Error => string.Empty;
+        public string this[string PropertyName]
+        {
+            get
+            {
+                CollectErrors();
+
+                return Errors.ContainsKey(PropertyName) ? Errors[PropertyName] : string.Empty;
+            }
+        }
+        private static List<PropertyInfo> _propertyInfos;
+        protected List<PropertyInfo> PropertyInfos
+        {
+            get
+            {
+                if (_propertyInfos == null)
+                {
+                    Trace.TraceInformation("Sammel type Infos");
+                    _propertyInfos =
+                        GetType()
+                        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .Where(prop => prop.IsDefined(typeof(RequiredAttribute), true) || prop.IsDefined(typeof(MinLengthAttribute), true)) // DAtaAnnotation
+                        .ToList();
+                }
+                return _propertyInfos;
+            }
+        }
+
+        private void CollectErrors()
+        {
+            Errors.Clear();
+            foreach (PropertyInfo prop in PropertyInfos)
+            {
+                var currentValue = prop.GetValue(this);
+                var requiredAttr = prop.GetCustomAttribute<RequiredAttribute>();
+                var minLenAttr = prop.GetCustomAttribute<MinLengthAttribute>();
+                if (requiredAttr != null)
+                {
+                    if (string.IsNullOrEmpty(currentValue?.ToString() ?? string.Empty))
+                    {
+                        Errors.Add(prop.Name, requiredAttr.ErrorMessage);
+                    }
+                }
+                if (minLenAttr != null)
+                {
+                    if ((currentValue?.ToString() ?? string.Empty).Length < minLenAttr.Length)  // ?? wenn das links von ?? nicht null ist dann dann links sonst rechts.
+                    {
+                        if (Errors.ContainsKey(prop.Name)) continue;
+                        Errors.Add(prop.Name, minLenAttr.ErrorMessage);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
--- Pw.Logik/ViewModel/MainViewModel.cs
+++ Pw.Logik/ViewModel/MainViewModel.cs
@@ -1,34 +1,99 @@
 using GalaSoft.MvvmLight;
+using Logik.Pw.Logik.Items;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace Logik.Wo.Logik.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
-    public class MainViewModel : ViewModelBase
-    {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        public MainViewModel()
+
+        public class MainViewModel : ViewModelBase
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+
+            public ObservableCollection<PwEintrag> MainListe { get; set; }
+            public PwEintrag AktEintrag
+            {
+                get
+                {
+                    var _tmpErg = UiViewListe?.CurrentItem as PwEintrag;
+                    if (_tmpErg != null)
+                    {
+                        // logik zum einzel anzeigen
+                    }
+                    return _tmpErg;
+                }
+                set
+                {
+                    UiViewListe.MoveCurrentTo(value);
+                    RaisePropertyChanged();
+                }
+            }
+
+            //  private ListCollectionView _UiViewListe;
+            public ListCollectionView UiViewListe { get; set; }
+
+
+
+            public MainViewModel()
+            {
+                if (IsInDesignMode)
+                {
+                    // Code runs in Blend --> create design time data.
+                    string Fehlermeldung = "";
+                }
+                MainListe = new ObservableCollection<PwEintrag>();
+                initzialise();
+            }
+
+
+            public void initzialise()
+            {
+
+                #region ListCollectionView Initailisierung
+
+                UiViewListe = CollectionViewSource.GetDefaultView(MainListe) as ListCollectionView;
+                foreach (var item in MainListe) // die vom System rein geladenen Daten müssen das OnPropertyChangeEvent "registrieren"
+                {
+                    item.PropertyChanged += PersonInfosPropertyAnders;
+                }
+                UiViewListe.CurrentChanged += (s, e) =>
+                {
+                    RaisePropertyChanged(() => AktEintrag);
+                };
+                MainListe.CollectionChanged += (s, e) =>
+                {
+                    if (e.NewItems != null)
+                    {
+                        foreach (INotifyPropertyChanged added in e.NewItems)
+                        {
+                            added.PropertyChanged += PersonInfosPropertyAnders;
+                        }
+                    }
+                    if (e.OldItems != null)
+                    {
+                        foreach (INotifyPropertyChanged wiederweg in e.OldItems)
+                        {
+                            wiederweg.PropertyChanged -= PersonInfosPropertyAnders;
+                        }
+                    }
+                };
+                #endregion
+
+                AktEintrag = UiViewListe?.CurrentItem as PwEintrag;
+            }
+
+            private void PersonInfosPropertyAnders(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == nameof(AktEintrag.HasErrors)) // würde die ganze zeit sonst beim initialisieren anschlagen und den RAM voll laufen
+                {
+                    return;
+                }
+                if (UiViewListe.IsEditingItem || UiViewListe.IsAddingNew) // nicht wechseln solange jemand darin rum schreibt
+                {
+                    return;
+                }
+                UiViewListe.Refresh();
+            }
+
         }
     }
-}
